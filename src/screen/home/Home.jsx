209c6359c40444adf66c935acf0eebde6d4baf88
Marco <zuck98@gmail.com>
2022-03-15 11:29:19 +0100

    creazione fetch api

--- src/screen/home/Home.jsx
+++ src/screen/home/Home.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { TextField } from '@mui/material'
-import background from ".../images/"
-
+/* import background from "./images/109666,jpg"
+ */
 
 function Home() {
+
+  const [filtro, setFiltro] = useState("");
+  const [zona, setZona] = useState([]);
+  const [previsioni, setPrevisioni] = useState([]);
+
+  const  fetchZona=() => {
+      fetch('http://api.weatherapi.com/v1/forecast.json?key=f656a3e4a7054753873211018220803&q='+ filtro + '&days=4')
+        .then(response => {
+          return response.json();
+        })
+        .then(data =>[
+          setZona(data.location)
+        ])
+        .then(data1 => {
+          setPrevisioni(data1.current)
+        })
+  };
+
+
+  useEffect(() => {
+    fetchZona();
+},[filtro])
+
+
   return (
-    <div className='container' style={{backgroundImage: `url(${background})`}}>
+    <div className='container' >
         <TextField
         style={{width: "100%"}}
           id="filled-textarea"
@@ -13,6 +37,10 @@ function Home() {
           placeholder="Città"
           multiline
           variant="filled"
+          value={filtro}
+          onChange={(ev) => {
+            setFiltro(ev.target.value);
+        }}
         />
     </div>
   )
